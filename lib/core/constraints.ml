3c5578323c6db191c67e33f53e1324a18ff53d73
Moeed Dar <moeed.dar@gmail.com>
2026-09-26 18:42:31 +0100

    simplify constraints rigid

--- lib/core/constraints.ml
+++ lib/core/constraints.ml
@@ -6,8 +6,6 @@ type t = t' Span.located
 
 let rigid = function
   | Conversion (a, b) ->
-      (not (Term.Meta_variable.contains a))
-      && not (Term.Meta_variable.contains b)
+      not (Term.Meta_variable.contains a || Term.Meta_variable.contains b)
   | Universe_inequality (a, b) ->
-      (not (Level.Meta_variable.contains a))
-      && not (Level.Meta_variable.contains b)
+      not (Level.Meta_variable.contains a || Level.Meta_variable.contains b)
